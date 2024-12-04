08b526888944f39757eeea550fee130ca481ebef
akiko04m <amkoli2@docomo.ne.jp>
2024-12-04 17:47:18 +0900

    11/19

--- app/Models/cocktails.php
+++ app/Models/cocktails.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class cocktails extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+    'title',
+    'body',
+    'includes_id'];
+
+    public function includes(){
+    return $this->belongsToMany(Includealchole::class);
+}
 }
--- resources/views/include/create.blade.php
+++ resources/views/include/create.blade.php
@@ -4,7 +4,14 @@
       @csrf
       <input type="text" name = include[name] />
       <input type="number" name = include[strange] />
-
+      <div class="category">
+        <h2>Category</h2>
+          <select name="post[category_id]">
+              @foreach($includes as $includes)
+                  <option value="{{ $includes->id }}">{{ $includes->name }}</option>
+              @endforeach
+          </select>
+      </div>
       <input type="submit" />
     </form>
   </div>
