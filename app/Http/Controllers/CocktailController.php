ed3677826dcceaed0aeccb5b729ec050e8754bfe
akiko04m <amkoli2@docomo.ne.jp>
2024-12-28 17:44:05 +0900

    ittan

--- app/Http/Controllers/CocktailController.php
+++ app/Http/Controllers/CocktailController.php
@@ -8,7 +8,7 @@ use App\Models\Cocktail;
 class CocktailController extends Controller
 {
     public function cocktail(Cocktail $cocktails){
-        return view('include.\cocktail')->with(['cocktails' => $cocktails->get()]);
+        return view('include/cocktail')->with(['cocktails' => $cocktails->get()]);
         return redirect('/cocktails');
     }//今回のindex = 一番最初に見られるファイル大元
 }
